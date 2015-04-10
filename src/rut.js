55cd019116e3986861e4127bc5cf3fa301d9d86e
René A. Morales <rene.morales.sanchez@gmail.com>
2015-04-10 12:23:36 -0300

    fix(): Prevent caret from moving to the end of input

--- src/rut.js
+++ src/rut.js
@@ -1,9 +1,12 @@
+var CLEAN_REGEX = /[^0-9kK]+/g;
+var INPUT_REGEX = /[^0-9kK.-]+/g;
+
 function cleanRut(_value) {
-  return typeof _value === 'string' ? _value.replace(/[^0-9kK]+/g,'').toUpperCase() : '';
+  return typeof _value === 'string' ? _value.replace(CLEAN_REGEX,'').toUpperCase() : '';
 }
 
 function removeInvalidCharacters(_value) {
-  return typeof _value === 'string' ? _value.replace(/[^0-9kK.-]+/g,'') : '';
+  return typeof _value === 'string' ? _value.replace(INPUT_REGEX,'') : '';
 }
 
 function formatRut(_value, _default) {
@@ -50,12 +53,16 @@ function addValidatorToNgModel(ngModel){
   ngModel.$formatters.unshift(validateAndFormat);
 }
 
-function removeInvalidCharactersOnWatch($scope, ngModel) {
+function removeInvalidCharactersOnWatch($scope, $element, ngModel) {
   $scope.$watch(function() {
     return ngModel.$viewValue;
   }, function() {
+    if ( ngModel.$viewValue && !ngModel.$viewValue.match(INPUT_REGEX) ) return; // String is clean, no need to replace it
+    var caretPosition = $element[0].selectionStart-1;
     ngModel.$setViewValue(removeInvalidCharacters(ngModel.$viewValue));
     ngModel.$render();
+    $element[0].selectionStart = caretPosition;
+    $element[0].selectionEnd = caretPosition;
   });
 }
 
@@ -88,7 +95,7 @@ angular.module('platanus.rut', [])
 
         addValidatorToNgModel(ngModel);
 
-        removeInvalidCharactersOnWatch($scope, ngModel);
+        removeInvalidCharactersOnWatch($scope, $element, ngModel);
 
         switch($attrs.rutFormat) {
         case 'live':
